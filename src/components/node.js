29966f3bd8ad46c793155e5d06ef611af32f1e47
Jake Tompkins <jaketompkins91@outlook.com>
2019-05-17 11:58:15 +0800

    added color and animation styling

--- src/components/node.js
+++ src/components/node.js
@@ -5,7 +5,7 @@ class Node extends Component {
     const { passable, start, end, passed, distance } = this.props
     return (
       <div className={`node${passable ? "" : " unpassable"}${start ? " start" : ""}${end ? " end" : ""}${passed ? " passed" : ""}`}
-        onClick={this.props.onClick} >{distance}</div>
+        onClick={this.props.onClick} />
     )
   }
 }
